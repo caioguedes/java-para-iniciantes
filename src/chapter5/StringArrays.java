b7a29ae78e75df0ad96d9ddf98f1ec23212d7a23
Caio Guedes <caio.cesar.g.souza@gmail.com>
2017-07-29 18:58:48 -0300

    Adding Strings Classes

---
+++ src/chapter5/StringArrays.java
@@ -0,0 +1,22 @@
+package chapter5;
+
+public class StringArrays {
+
+    public static void main(String[] args) {
+        String[] strs = {"This", "is", "a", "test."};
+
+        System.out.println("Original Array:");
+        for (String s : strs) {
+            System.out.print(s + " ");
+        }
+        System.out.println("\n");
+
+        strs[1] = "was";
+        strs[3] = "test, too!";
+
+        System.out.print("Modified array:");
+        for (String s : strs) {
+            System.out.print(s + " ");
+        }
+    }
+}
