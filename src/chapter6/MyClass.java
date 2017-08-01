b79b4e5f9b6bc7d9367d142bf265e23efa2b9a07
Caio Guedes <caio.cesar.g.souza@gmail.com>
2017-08-01 13:19:35 -0300

    Adding more classes...

---
+++ src/chapter6/MyClass.java
@@ -0,0 +1,16 @@
+package chapter6;
+
+public class MyClass {
+
+    private int alpha;
+    public int beta;
+    int gamma;
+
+    void setAlpha(int a) {
+        alpha = a;
+    }
+
+    int getAlpha() {
+        return alpha;
+    }
+}
