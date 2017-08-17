cec1b4aff70dfb92eba934e48cd157e6914bf145
Caio Guedes <caio.cesar.g.souza@gmail.com>
2017-08-17 18:38:30 -0300

    Adding DynDispDemo

---
+++ src/chapter7/Sub2.java
@@ -0,0 +1,7 @@
+package chapter7;
+
+public class Sub2 extends Sup {
+    void who() {
+        System.out.println("who() in Sub2");
+    }
+}
---
+++ src/chapter7/Sup.java
@@ -0,0 +1,7 @@
+package chapter7;
+
+public class Sup {
+    void who() {
+        System.out.println("who() in Sup");
+    }
+}
