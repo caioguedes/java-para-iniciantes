c883c821701a56ec0909787974c79c7ec4c3e748
Caio Guedes <caio.cesar.g.souza@gmail.com>
2017-08-17 13:42:08 -0300

    Adding Shapes Classes

---
+++ src/chapter7/Rectangle.java
@@ -0,0 +1,12 @@
+package chapter7;
+
+public class Rectangle extends TwoDShape {
+    boolean isSquare() {
+        if (width == height) return true;
+        return false;
+    }
+
+    double area() {
+        return width * height;
+    }
+}
