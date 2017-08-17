2ac1abd05196d498506b9970d701a55e2d8cad56
Caio Guedes <caio.cesar.g.souza@gmail.com>
2017-08-17 17:47:38 -0300

    Adding More Classes

---
+++ src/chapter7/A.java
@@ -0,0 +1,5 @@
+package chapter7;
+
+class A {
+    int i;
+}
---
+++ src/chapter7/UseSuper.java
@@ -0,0 +1,8 @@
+package chapter7;
+
+public class UseSuper {
+    public static void main(String[] args) {
+        B subObj = new B(1, 2);
+        subObj.show();
+    }
+}
