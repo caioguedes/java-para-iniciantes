79b118663ef3d8c7574fb28d7fb81436e878ba30
Caio Guedes <caio.cesar.g.souza@gmail.com>
2017-08-18 14:32:53 -0300

    Updating UseBook

--- src/chapter8/bookpackext/UseBook.java
+++ src/chapter8/bookpackext/UseBook.java
@@ -1,14 +1,16 @@
 package chapter8.bookpackext;
 
+import chapter8.bookpack.Book;
+
 class UseBook {
     public static void main(String[] args) {
-        chapter8.bookpack.Book books[] = new chapter8.bookpack.Book[5];
+        Book books[] = new Book[5];
 
-        books[0] = new chapter8.bookpack.Book("Java: A Beginner's Guide", "Schildt", 2014);
-        books[1] = new chapter8.bookpack.Book("Java; The Complete Reference", "Schildt", 2014);
-        books[2] = new chapter8.bookpack.Book("The Art Of Java", "Schildt and Holmes", 2003);
-        books[3] = new chapter8.bookpack.Book("Red Storm Resing", "Clancy", 1986);
-        books[4] = new chapter8.bookpack.Book("On the Road", "Kerouac", 1955);
+        books[0] = new Book("Java: A Beginner's Guide", "Schildt", 2014);
+        books[1] = new Book("Java; The Complete Reference", "Schildt", 2014);
+        books[2] = new Book("The Art Of Java", "Schildt and Holmes", 2003);
+        books[3] = new Book("Red Storm Resing", "Clancy", 1986);
+        books[4] = new Book("On the Road", "Kerouac", 1955);
 
         for (int i = 0; i < books.length; i++)  books[i].show();
     }
