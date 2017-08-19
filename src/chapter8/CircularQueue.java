b3de18e9eb568615b792e683ea317c48272776bc
Caio Guedes <caio.cesar.g.souza@gmail.com>
2017-08-18 23:14:56 -0300

    Adding IQDemo

---
+++ src/chapter8/CircularQueue.java
@@ -0,0 +1,32 @@
+package chapter8;
+
+public class CircularQueue implements ICharQ {
+    private char q[];
+    private int putloc, getloc;
+
+    public CircularQueue(int size) {
+        q = new char[size+1];
+        putloc = getloc = 0;
+    }
+
+    public void put(char ch) {
+        if (putloc + 1 == getloc | ((putloc == q.length - 1) & (getloc == 0))) {
+            System.out.println(" - Queue is full");
+            return;
+        }
+
+        q[putloc++] = ch;
+        if (putloc == q.length) putloc = 0;
+    }
+
+    public char get() {
+        if (getloc == putloc) {
+            System.out.println(" - Queue is empty.");
+            return (char) 0;
+        }
+
+        char ch = q[getloc++];
+        if (getloc == q.length) getloc = 0;
+        return ch;
+    }
+}
---
+++ src/chapter8/FixedQueue.java
@@ -0,0 +1,28 @@
+package chapter8;
+
+public class FixedQueue implements ICharQ {
+    private char[] q;
+    private int putloc, getloc;
+
+    public FixedQueue(int size) {
+        q = new char[size];
+        putloc = getloc = 0;
+    }
+
+    public void put(char ch) {
+        if (putloc == q.length) {
+            System.out.println(" - Queue is full");
+            return;
+        }
+        q[putloc++] = ch;
+    }
+
+    public char get() {
+        if (getloc == putloc) {
+            System.out.println(" - Queue is empty.");
+            return (char) 0;
+        }
+
+        return q[getloc++];
+    }
+}
