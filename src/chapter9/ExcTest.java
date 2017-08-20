c9803bcba729b12207ea8f8b29b67674ba448d7a
Caio Guedes <caio.cesar.g.souza@gmail.com>
2017-08-20 16:11:37 -0300

    Adding ExcDemo1

---
+++ src/chapter9/ExcTest.java
@@ -0,0 +1,10 @@
+package chapter9;
+
+public class ExcTest {
+    static void genException() {
+        int[] nums = new int[4];
+        System.out.println("Before exception is generated.");
+        nums[7]  = 10;
+        System.out.println("this won't be displayed.");
+    }
+}
