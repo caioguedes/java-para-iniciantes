aabdf21069845f9a59dd3507bc7757e998cca7ef
Caio Guedes <caio.cesar.g.souza@gmail.com>
2017-08-25 13:25:47 -0300

    Adding CustomExceptionDemo

---
+++ src/chapter9/NonIntResultException.java
@@ -0,0 +1,15 @@
+package chapter9;
+
+public class NonIntResultException extends Exception {
+    int n;
+    int d;
+
+    NonIntResultException(int i, int j) {
+        n = i;
+        d = j;
+    }
+
+    public String toString() {
+        return "Result of " + n + " / " + d + " is non-integer.";
+    }
+}
