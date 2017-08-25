352341ed10821ac87b2720d00846aacc82765f4b
Caio Guedes <caio.cesar.g.souza@gmail.com>
2017-08-25 14:20:48 -0300

    Adding WriteDemo

---
+++ src/chapter10/WriteDemo.java
@@ -0,0 +1,11 @@
+package chapter10;
+
+public class WriteDemo {
+    public static void main(String[] args) {
+        int b;
+        b = 'X';
+
+        System.out.print(b);
+        System.out.print('\n');
+    }
+}
