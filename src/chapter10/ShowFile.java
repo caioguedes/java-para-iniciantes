6a55cff30bea5b4b40982f529f84066b51274e1e
Caio Guedes <caio.cesar.g.souza@gmail.com>
2017-08-27 13:42:37 -0300

    Refactoring ShowFile

--- src/chapter10/ShowFile.java
+++ src/chapter10/ShowFile.java
@@ -5,24 +5,21 @@ import java.io.*;
 public class ShowFile {
     public static void main(String[] args) {
         int i;
-        FileInputStream fin = null;
 
-        try {
-            fin = new FileInputStream(args[0]);
+        if (args.length != 1) {
+            System.out.println("Usage: ShowFile filename");
+            return;
+        }
+
+        try (FileInputStream fin = new FileInputStream(args[0])) {
 
             do {
                 i = fin.read();
-                if (i != -1) System.out.print((char) i);
+                if (i != -1) System.out.print(i);
             } while (i != -1);
 
         } catch (IOException e) {
-            System.out.println("I/O Error: " + e);
-        } finally {
-            try {
-                fin.close();
-            } catch (IOException e) {
-                System.out.println("Error closing file.");
-            }
+            e.printStackTrace();
         }
     }
 }
