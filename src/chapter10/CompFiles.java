bad9e6f6d2b1e01bf390f38c07e93dfc1d226110
Caio Guedes <caio.cesar.g.souza@gmail.com>
2017-08-29 19:42:12 -0300

    Adding CompFiles

---
+++ src/chapter10/CompFiles.java
@@ -0,0 +1,34 @@
+package chapter10;
+
+import java.io.*;
+
+public class CompFiles {
+    public static void main(String[] args) {
+        int i = 0, j = 0;
+
+        if (args.length != 2) {
+            System.out.println("Usage: CompFiles f1 f2");
+            return;
+        }
+
+        try (
+            FileInputStream f1 = new FileInputStream(args[0]);
+            FileInputStream f2 = new FileInputStream(args[2])
+        ) {
+            do {
+                i = f1.read();
+                j = f2.read();
+
+                if (i != j) break;
+            } while (i != -1 && j != -1);
+
+            if (i != j) {
+                System.out.println("Files differ.");
+            } else {
+                System.out.println("Files are the same.");
+            }
+        } catch (IOException e) {
+            System.out.println("I/O Error: " + e);
+        }
+    }
+}
