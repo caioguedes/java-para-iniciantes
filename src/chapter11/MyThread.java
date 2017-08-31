4ea6e61e15aca57ff82e994baaf9e5981c8d4996
Caio Guedes <caio.cesar.g.souza@gmail.com>
2017-08-31 12:24:33 -0300

    Adding UseThreadImproved

--- src/chapter11/MyThread.java
+++ src/chapter11/MyThread.java
@@ -1,22 +1,23 @@
 package chapter11;
 
 public class MyThread implements Runnable {
-    String thrdName;
+    Thread thrd;
 
     MyThread(String name) {
-        thrdName = name;
+        thrd = new Thread(this, name);
+        thrd.start();
     }
 
     public void run() {
-        System.out.println(thrdName + " starting.");
+        System.out.println(thrd.getName() + " starting.");
         try {
             for (int count = 0; count < 10; count++) {
                 Thread.sleep(400);
-                System.out.println("In " + thrdName + ", count is " + count);
+                System.out.println("In " + thrd.getName() + ", count is " + count);
             }
         } catch (InterruptedException e) {
-            System.out.println(thrdName + " interrupted.");
+            System.out.println(thrd.getName() + " interrupted.");
         }
-        System.out.println(thrdName + " terminating.");
+        System.out.println(thrd.getName() + " terminating.");
     }
 }
---
+++ src/chapter11/UseThreadsImproved.java
@@ -0,0 +1,20 @@
+package chapter11;
+
+public class UseThreadsImproved {
+    public static void main(String[] args) {
+        System.out.println("Main thread starting.");
+
+        MyThread mt = new MyThread("Child #1");
+
+        for (int i = 0; i < 50; i++) {
+            System.out.print(".");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                System.out.println("Main thread interrupted.");
+            }
+        }
+
+        System.out.println("Main thread ending.");
+    }
+}
