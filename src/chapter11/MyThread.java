5d2a5ae4af39e70b6d6e4f34979c87a7c24126d6
Caio Guedes <caio.cesar.g.souza@gmail.com>
2017-08-31 12:29:55 -0300

    Adding ExtendedThread

--- src/chapter11/MyThread.java
+++ src/chapter11/MyThread.java
@@ -1,23 +1,22 @@
 package chapter11;
 
-public class MyThread implements Runnable {
-    Thread thrd;
+public class MyThread extends Thread {
 
     MyThread(String name) {
-        thrd = new Thread(this, name);
-        thrd.start();
+        super(name);
+        start();
     }
 
     public void run() {
-        System.out.println(thrd.getName() + " starting.");
+        System.out.println(getName() + " starting.");
         try {
             for (int count = 0; count < 10; count++) {
                 Thread.sleep(400);
-                System.out.println("In " + thrd.getName() + ", count is " + count);
+                System.out.println("In " + getName() + ", count is " + count);
             }
         } catch (InterruptedException e) {
-            System.out.println(thrd.getName() + " interrupted.");
+            System.out.println(getName() + " interrupted.");
         }
-        System.out.println(thrd.getName() + " terminating.");
+        System.out.println(getName() + " terminating.");
     }
 }
