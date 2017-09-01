b210c71fd3c5b185d003b24a9cb8db013d82846f
Caio Guedes <caio.cesar.g.souza@gmail.com>
2017-09-01 12:08:52 -0300

    Adding PriorityDemo

---
+++ src/chapter11/PriorityDemo.java
@@ -0,0 +1,24 @@
+package chapter11;
+
+public class PriorityDemo {
+    public static void main(String[] args) {
+        Priority mt1 = new Priority("High Priority");
+        Priority mt2 = new Priority("Low Priority");
+
+        mt1.thrd.setPriority(Thread.NORM_PRIORITY + 2);
+        mt2.thrd.setPriority(Thread.NORM_PRIORITY - 2);
+
+        mt1.thrd.start();
+        mt2.thrd.start();
+
+        try {
+            mt1.thrd.join();
+            mt2.thrd.join();
+        } catch (InterruptedException e) {
+            System.out.println("Main thread interrupted.");
+        }
+
+        System.out.println("\nHigh priority thread counted to " + mt1.count);
+        System.out.println("\nLow priority thread counted to " + mt2.count);
+    }
+}
