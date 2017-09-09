7a61b4c8088b5e5243451cdc700fac97146d3d0a
Caio Guedes <caio.cesar.g.souza@gmail.com>
2017-09-09 15:15:02 -0300

    Adding Quadratic

---
+++ src/chapter12/Quadratic.java
@@ -0,0 +1,17 @@
+package chapter12;
+
+public class Quadratic {
+    public static void main(String[] args) {
+        double a, b, c, x;
+
+        a = 4;
+        b = 1;
+        c = -3;
+
+        x = (-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+        System.out.println("First solution: " + x);
+
+        x = (-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+        System.out.println("Second solution: " + x);
+    }
+}
